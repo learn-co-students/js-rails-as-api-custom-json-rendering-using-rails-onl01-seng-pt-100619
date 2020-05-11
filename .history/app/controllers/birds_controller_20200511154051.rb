e91c73b1abc8f708f1f02a12044260459a79ffd6
steelerx155 <steelerx155@gmail.com>
2020-05-11 16:14:53 -0400

    Done.

---
+++ .history/app/controllers/birds_controller_20200511154051.rb
@@ -0,0 +1,13 @@
+class BirdsController < ApplicationController
+  class BirdsController < ApplicationController
+    def index
+      birds = Bird.all
+      render json: birds
+    end
+   
+    def show
+      bird = Bird.find_by(id: params[:id])
+      render json: bird
+    end
+  end
+end
---
+++ .history/app/controllers/birds_controller_20200511160459.rb
@@ -0,0 +1,13 @@
+class BirdsController < ApplicationController
+ 
+    def index
+      birds = Bird.all
+      render json: birds
+    end
+   
+    def show
+      bird = Bird.find_by(id: params[:id])
+      render json: {id: bird.id, name: bird.name, species: bird.species }
+    end
+  
+end
